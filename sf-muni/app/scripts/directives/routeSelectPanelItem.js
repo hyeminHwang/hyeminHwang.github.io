96f941adf31e19627c300fe2656d522540888ad6
Hwang, Hyemin <hyemin@gatech.edu>
2017-10-21 23:58:05 -0400

    SF-MUNI, add li background color when user select route

--- sf-muni/app/scripts/directives/routeSelectPanelItem.js
+++ sf-muni/app/scripts/directives/routeSelectPanelItem.js
@@ -6,12 +6,15 @@
       return {
         restrict: 'E',
         scope: false,
-        template: '<li class="nav-item" ><a class="nav-link" href="" ng-click="addToFilterList(data)">{{data}}</a></li>',
+        template: "<li class='nav-item' id='li-{{data}}' ><a class='nav-link' href='' ng-click='addToFilterList(data)'>{{data}}</a></li>",
         replace: true,
         link: function (scope, element, attrs) {
           scope.data = attrs.data;
 
           scope.addToFilterList = function(tag) {
+            var liStyleElement = document.getElementById('li-' + tag).style;
+            if (liStyleElement.backgroundColor == "") liStyleElement.backgroundColor = "#7F7FFF";
+            else liStyleElement.backgroundColor = "";
 
             var routeFilterList = scope.$root.routeFilterList
             if (!routeFilterList) routeFilterList = [];
